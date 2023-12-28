630d25e7a2bdab6c28872de130e7fa01c4e1ddd0
Bui-Thanh-Liem <buithanhliem5073@gmail.com>
2023-12-28 09:27:07 +0700

    Theming

---
+++ src/theme.js
@@ -0,0 +1,27 @@
+import { createTheme } from '@mui/material/styles';
+import { red, pink, orange } from '@mui/material/colors';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#556cd6'
+    },
+    secondary: {
+      main: '#f48fb1'
+    },
+    error: {
+      main: '#f48fb1'
+    },
+    warning: {
+      main: '#f48fb1'
+    },
+    text: {
+      primary: '#f48fb1',
+      secondary: pink
+    }
+  }
+});
+
+export default theme;
