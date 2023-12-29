45735c5f201878a6d1a6962d196acf66184b9c0e
Bui-Thanh-Liem <buithanhliem5073@gmail.com>
2023-12-29 16:55:14 +0700

    completed_layout_app

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,42 @@
-import Button from '@mui/material/Button';
-import { Home } from '@mui/icons-material';
-import { pink } from '@mui/material/colors';
-import Typography from '@mui/material/Typography';
+import { Container, Box } from '@mui/material';
 
 import DarkModeSelect from './DarkMode';
 
 function App() {
   return (
-    <>
-      <div>
+    <Container
+      disableGutters
+      maxWidth={false}
+      sx={{ height: '100vh' }}
+    >
+      <Box sx={{
+        width: '100%',
+        height: (theme) => theme.customTrello.appBarHeight,
+        bgcolor: 'primary.light',
+        display: 'flex',
+        alignItems: 'center'
+      }}>
         <DarkModeSelect />
-      </div>
-      <br />
-      <h1>Bui Thanh Liem</h1>
-      <Button variant="contained" color="primary">
-        Hello world
-      </Button>
-      <br />
-      <Button variant="outlined">Hello world</Button>
-      <br />
-      <Button variant="contained" color="secondary">
-        Hello world
-      </Button>
-      <br />
-      <Button variant="contained" color="success">
-        Success
-      </Button>
-      <br />
-      <Button variant="contained" color="error">
-        Error
-      </Button>
-      <Button variant="contained" color="warning">
-        Warning
-      </Button>
-
-      <Typography variant="body2" color="text.secondary">
-        The Material UI theme will be separated from the other library, so when
-        you use APIs such as styled, the sx prop, and useTheme, you ll be able
-        to access Material UI s theme like you normally would.
-      </Typography>
-      <Home color="primary" />
-      <Home color="secondary" />
-      <Home color="success" />
-      <Home color="action" />
-      <Home color="disabled" />
-      <Home sx={{ color: pink[500] }} />
-    </>
+      </Box>
+      <Box sx={{
+        width: '100%',
+        height: (theme) => theme.customTrello.appBoardHeight,
+        bgcolor: 'primary.dark',
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        App-Board
+      </Box>
+      <Box sx={{
+        width: '100%',
+        height: (theme) => `calc(100vh - ${theme.customTrello.appBarHeight} - ${theme.customTrello.appBoardHeight})`,
+        bgcolor: 'primary.main',
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        App-Content
+      </Box>
+    </Container>
   );
 }
 
--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,10 @@ import { teal, deepOrange, cyan, orange } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = extendTheme({
+  customTrello: {
+    appBarHeight: '48px',
+    appBoardHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
