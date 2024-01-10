33947cb1a62da192bf1b0c0f1c7ac17349ea3e3c
Bui-Thanh-Liem <buithanhliem5073@gmail.com>
2024-01-10 09:00:24 +0700

    completed-appBar-ui-basic

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,20 +1,78 @@
-import { Box } from '@mui/material';
+import {
+  Box,
+  Typography,
+  Button,
+  TextField,
+  Badge,
+  Tooltip
+} from '@mui/material';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import SvgIcon from '@mui/material/SvgIcon';
 
-import DarkModeSelect from '../DarkMode';
+import DarkModeSelect from '~/components/DarkMode';
+import trelloIcon from '~/assets/trello.svg?react';
+import Workspaces from './menus/Workspaces';
+import Recent from './menus/Recent';
+import Starred from './menus/Starred';
+import Templates from './menus/Templates';
+import Profiles from './menus/Profiles';
 
 const AppBar = () => {
   return (
     <Box
+      px={2}
       sx={{
         width: '100%',
         height: (theme) => theme.customTrello.appBarHeight,
-        bgcolor: 'primary.light',
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between'
       }}
     >
-      <DarkModeSelect />
-      App Bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <AppsIcon sx={{ color: 'primary.main' }} />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon component={trelloIcon} sx={{ color: 'primary.main' }} />
+          <Typography
+            variant="span"
+            sx={{
+              fontSize: '1.2rem',
+              fontWeight: 'bold',
+              color: 'primary.main'
+            }}
+          >
+            Trello
+          </Typography>
+        </Box>
+        <Workspaces />
+        <Recent />
+        <Starred />
+        <Templates />
+        <Button variant="contained">Create</Button>
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Tooltip title="Search /">
+          <TextField
+            id="outlined-basic-search"
+            label="Search..."
+            size="small"
+            variant="outlined"
+          />
+        </Tooltip>
+        <DarkModeSelect />
+        <Tooltip title="Notifications">
+          <Badge color="secondary" variant="dot">
+            <NotificationsNoneIcon />
+          </Badge>
+        </Tooltip>
+        <Tooltip title="Informations">
+          <HelpOutlineIcon />
+        </Tooltip>
+        <Profiles />
+      </Box>
     </Box>
   );
 };
